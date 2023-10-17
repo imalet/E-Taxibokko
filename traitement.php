d8069da487548fa9b8c957583a659919c0668cd2
IMALET Benji <imaletbenji58@gmail.com>
2023-10-18 00:08:53 +0200

    Last commit

--- traitement.php
+++ traitement.php
@@ -2,7 +2,7 @@
 
 
 try {
-    $db = new PDO('mysql:host=localhost;dbname=taxibokko','root','');
+    $db = new PDO('mysql:host=localhost;dbname=taxibokko', 'root', '');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     throw $e;
@@ -10,21 +10,30 @@ try {
 
 if (isset($_POST['valider'])) {
 
-    $nom = $_POST['nom'];
+    $nom = htmlspecialchars($_POST['prenom'], ENT_QUOTES, 'UTF-8');
     $prenom = $_POST['prenom'];
-    $mdp = hash('sha256',$_POST['password']);
-    $email = $_POST['email'];
+    $mdp = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
 
-    $req = $db->prepare('INSERT INTO users(nom, prenom, motdepasse, email) VALUES(:nom, :prenom, :motdepasse, :email)');
-    $req->bindParam(':nom',$nom);
-    $req->bindParam(':prenom',$prenom);
-    $req->bindParam(':motdepasse',$mdp);
-    $req->bindParam(':email',$email);
+    $sele = $db->prepare("SELECT * FROM users WHERE email = :email");
+    $sele->bindParam(':email', $email);
+    $sele->execute();
 
-    if ($req->execute()) {
-        echo "Insertion reussi";
-    }else {
-        echo "Insertion Echoué";
+    $resultat = $sele->fetch(PDO::FETCH_ASSOC);
+
+    if ($resultat['email'] === $email) {
+        echo "Il existe";
+    } else {
+        $req = $db->prepare('INSERT INTO users(nom, prenom, motdepasse, email) VALUES(:nom, :prenom, :motdepasse, :email)');
+        $req->bindParam(':nom', $nom);
+        $req->bindParam(':prenom', $prenom);
+        $req->bindParam(':motdepasse', $mdp);
+        $req->bindParam(':email', $email);
+
+        if ($req->execute()) {
+            echo "Insertion reussi";
+        } else {
+            echo "Insertion Echoué";
+        }
     }
-
 }
